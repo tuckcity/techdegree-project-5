b6cb98ee5819fc2856c33f878f55a53cd0e1bb73
Alex Tucker <tucker.ale@gmail.com>
2023-07-20 13:09:11 +0700

    search filter attempt

--- js/app.js
+++ js/app.js
@@ -1,4 +1,18 @@
 baguetteBox.run('.grid');
-const search = new Filter('search', 'data-caption');
+// const search = new Filter('search', 'data-caption');
 
 
+// Search Filter original code
+
+const search = document.getElementById(`search`);
+search.addEventListener('keyup', e => {
+    let searchValue = e.target.value.toLowerCase();
+    let cards = document.querySelectorAll('a.imgcard');
+    cards.forEach(card => {
+        if (card.textContent.toLocaleLowerCase().includes(searchValue)) {
+            card.parentNode.style.display = 'grid';
+        } else {
+            card.parentNode.style.display = 'none';
+        }
+    });
+});
